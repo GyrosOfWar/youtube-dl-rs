98a70190277e0b1ea13fac183de9e8dbe2520d96
Martin Tomasi <martin.tomasi@gmail.com>
2022-04-25 16:57:26 +0200

    improve examples

--- examples/downloader.rs
+++ examples/downloader.rs
@@ -1,9 +1,11 @@
-use youtube_dl::YoutubeDl;
+use youtube_dl::{download_yt_dlp, YoutubeDl};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let yt_dlp_path = download_yt_dlp(".").await?;
+
     let output = YoutubeDl::new("https://www.youtube.com/watch?v=VFbhKZFzbzk")
-        .socket_timeout("15")
+        .youtube_dl_path(yt_dlp_path)
         .run_async()
         .await?;
     let title = output.into_single_video().unwrap().title;
