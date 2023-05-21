c5369418b72d606ff64fd526d69c95f6599986f0
Shayne Hartford <shaybox@shaybox.com>
2023-05-21 11:25:01 -0400

    Add custom deserializer to simplify types

--- src/model.rs
+++ src/model.rs
@@ -146,7 +146,8 @@ pub struct JsonOutput {
 
 #[derive(Clone, Serialize, Deserialize, Debug, Default)]
 pub struct Playlist {
-    pub entries: Option<Vec<Option<SingleVideo>>>,
+    #[serde(default, deserialize_with = "parse_entries")]
+    pub entries: Option<Vec<SingleVideo>>,
     pub extractor: Option<String>,
     pub extractor_key: Option<String>,
     pub id: Option<String>,
@@ -321,12 +322,25 @@ pub enum Protocol {
 // given as "none" (instead of simply missing from the JSON).
 // Default decoding in this case would result in `Some("none".to_string())`, which is why
 // this custom parse function exists.
-fn parse_codec<'de, D>(d: D) -> Result<Option<String>, D::Error>
+fn parse_codec<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
 where
     D: Deserializer<'de>,
 {
-    Deserialize::deserialize(d).map(|x: Option<_>| match x.unwrap_or_default() {
+    Deserialize::deserialize(deserializer).map(|x: Option<_>| match x.unwrap_or_default() {
         Some(s) if s == "none" => None,
         x => x,
     })
+}
+
+// Video entries can be null in the case of premium videos
+// Flattens entries to simplify the type from Option<Vec<Option<SingleVideo>>>> to Option<Vec<SingleVideo>>
+fn parse_entries<'de, D>(deserializer: D) -> Result<Option<Vec<SingleVideo>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let entries: Option<Vec<Option<SingleVideo>>> = Deserialize::deserialize(deserializer)?;
+    let flattened_entries = entries
+        .map(|vec_option_single_video| vec_option_single_video.into_iter().flatten().collect());
+
+    Ok(flattened_entries)
 }
